5bb589004fef1539987ed4c8706707c35ce46c53
Leith Atia <leith.atia@gmail.com>
2022-08-19 10:53:31 +0100

    some stuff

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    # raise
+
+    # raise
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      # render :new, status: :unprocessable_entity
+    end
+    # raise
+  end
+
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
